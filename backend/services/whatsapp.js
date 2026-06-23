7db225afbc900c9cbccfa697a5f7f70fb989bb0d
Jickson-S <geojickson@gmail.com>
2026-06-23 21:01:00 +0530

    fix render chromium

--- backend/services/whatsapp.js
+++ backend/services/whatsapp.js
@@ -1,93 +1,53 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode");
 
-
 const client = new Client({
-
     authStrategy: new LocalAuth({
         clientId:"whatsapp-bot"
     }),
-
     puppeteer: {
-
         headless:true,
-
         executablePath:
         process.env.CHROME_PATH,
-
         args:[
             "--no-sandbox",
             "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage"
+            "--disable-dev-shm-usage",
+            "--disable-gpu"
         ]
-
     }
-
 });
 
-
 client.on("qr", async(qr)=>{
-
     const qrImage =
         await qrcode.toDataURL(qr);
-
-
     console.log("QR Generated");
-
-
     if(global.io){
-
         global.io.emit(
             "whatsapp-qr",
             qrImage
         );
-
     }
-
 });
-
 
 
 client.on("ready",()=>{
-
     console.log(
-        "WhatsApp Connected"
+        "WhatsApp Connected ✅"
     );
-
-
-    if(global.io){
-
-        global.io.emit(
-            "whatsapp-status",
-            {
-                status:"CONNECTED"
-            }
-        );
-
-    }
 
 });
 
-
-
-const sendMessage = async(number,message)=>{
-
-
+const sendMessage = async (number, message) => {
     await client.sendMessage(
         `${number}@c.us`,
         message
     );
-
-
 };
-
-
 
 client.initialize();
 
-
-
-module.exports={
+module.exports = {
     client,
     sendMessage
 };
