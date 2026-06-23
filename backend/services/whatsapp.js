d5dcc050f4329d0d65d283ad5c837cc828ac4f1b
Jickson-S <geojickson@gmail.com>
2026-06-23 20:07:38 +0530

    fix whatsapp puppeteer render

--- backend/services/whatsapp.js
+++ backend/services/whatsapp.js
@@ -1,19 +1,19 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode");
-const path = require("path");
+const puppeteer = require("puppeteer");
+
 
 const client = new Client({
 
-    authStrategy:new LocalAuth({
+    authStrategy: new LocalAuth({
         clientId:"whatsapp-bot"
     }),
 
-    puppeteer:{
+    puppeteer: {
 
         headless:true,
 
-        executablePath:
-        "/opt/render/.cache/puppeteer/chrome/linux-150.0.7871.24/chrome-linux64/chrome",
+        executablePath: puppeteer.executablePath,
 
         args:[
             "--no-sandbox",
@@ -26,15 +26,17 @@ const client = new Client({
 });
 
 
-// QR Generate
-client.on("qr", async (qr) => {
 
-    const qrImage = await qrcode.toDataURL(qr);
+client.on("qr", async(qr)=>{
+
+    const qrImage =
+        await qrcode.toDataURL(qr);
+
 
     console.log("QR Generated");
 
 
-    if (global.io) {
+    if(global.io){
 
         global.io.emit(
             "whatsapp-qr",
@@ -46,16 +48,15 @@ client.on("qr", async (qr) => {
 });
 
 
-// Connected
-client.on("ready", () => {
 
+client.on("ready",()=>{
 
     console.log(
-        "WhatsApp Connected ✅"
+        "WhatsApp Connected"
     );
 
 
-    if (global.io) {
+    if(global.io){
 
         global.io.emit(
             "whatsapp-status",
@@ -69,54 +70,15 @@ client.on("ready", () => {
 });
 
 
-// Authenticated
-client.on("authenticated", () => {
 
-    console.log(
-        "WhatsApp Authenticated"
-    );
-
-});
-
-
-// Disconnected
-client.on("disconnected", (reason)=>{
-
-    console.log(
-        "WhatsApp Disconnected",
-        reason
-    );
-
-});
-
-
-
-// Send Message Function
 const sendMessage = async(number,message)=>{
 
-    try {
 
+    await client.sendMessage(
+        `${number}@c.us`,
+        message
+    );
 
-        await client.sendMessage(
-            `${number}@c.us`,
-            message
-        );
-
-
-        console.log(
-            "Message Sent",
-            number
-        );
-
-
-    } catch(error){
-
-        console.log(
-            "Send Message Error",
-            error.message
-        );
-
-    }
 
 };
 
@@ -126,9 +88,7 @@ client.initialize();
 
 
 
-module.exports = {
-
+module.exports={
     client,
     sendMessage
-
 };
