bf639574e2ec222d9af0b7e7d4c63929ce37c208
Jickson-S <geojickson@gmail.com>
2026-06-24 12:14:33 +0530

    Remove 7

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,6 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 
-const { initializeClient } = require("./services/whatsapp");
-
-initializeClient()
-  .then(() => {
-    console.log("WhatsApp service started");
-  })
-  .catch((err) => {
-    console.log("WhatsApp start error:", err.message);
-  });
-
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const whatsappRoutes = require("./routes/whatsappRoutes");
@@ -24,49 +14,125 @@ const adminAuthRoutes = require("./routes/adminAuthRoutes");
 const http = require("http");
 const { Server } = require("socket.io");
 
+
 const app = express();
 
+
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin:[
+      "http://localhost:5173",
+      "https://whatsapp-otp-system.vercel.app"
+    ],
+    methods:["GET","POST"],
+    credentials:true
+  })
+);
+
 
 app.use(express.json());
+
+
 
 // database
 connectDB();
 
-// test api
-app.get("/", (req, res) => {
-  res.json({
-    message: "WhatsApp OTP Server Running",
-  });
-});
+
 
 // routes
 app.use("/api/auth", authRoutes);
+
 app.use("/api/user", userRoutes);
+
 app.use("/api/whatsapp", whatsappRoutes);
+
 app.use("/api/admin", adminUserRoutes);
+
 app.use("/api/admin", adminRoutes);
+
 app.use("/api/admin-auth", adminAuthRoutes);
 
-// socket
+
+
+// test
+app.get("/",(req,res)=>{
+
+    res.json({
+        message:"WhatsApp OTP Server Running"
+    });
+
+});
+
+
+
+// socket server
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173", "https://whatsapp-otp-system.vercel.app"],
-    methods: ["GET", "POST"],
-  },
+
+const io = new Server(server,{
+
+    cors:{
+        origin:[
+            "http://localhost:5173",
+            "https://whatsapp-otp-system.vercel.app"
+        ],
+        methods:["GET","POST"]
+    }
+
 });
 
+
+
+// IMPORTANT
 global.io = io;
 
-io.on("connection", (socket) => {
-  console.log("Socket Connected", socket.id);
+
+
+io.on("connection",(socket)=>{
+
+    console.log(
+        "Socket Connected:",
+        socket.id
+    );
+
 });
+
+
+
+// START WHATSAPP AFTER SOCKET READY
+
+const { initializeClient } = require("./services/whatsapp");
+
+
+initializeClient()
+.then(()=>{
+
+    console.log(
+        "WhatsApp service started"
+    );
+
+})
+.catch(err=>{
+
+    console.log(
+        "WhatsApp start error:",
+        err.message
+    );
+
+});
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+
+server.listen(PORT,()=>{
+
+    console.log(
+        `Server running on ${PORT}`
+    );
+
 });
